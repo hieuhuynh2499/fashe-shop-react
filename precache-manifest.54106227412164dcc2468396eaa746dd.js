4a3a86b720ede6b3a37c7d7c69de0d4970ccf5e1
unknown <hieuh5355@gmail.com>
2020-09-29 10:24:06 +0700

    Updates

---
+++ precache-manifest.54106227412164dcc2468396eaa746dd.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00afa938f1e0d2166e94a7ba3ee40a4f",
+    "url": "/fashe-shop-react/index.html"
+  },
+  {
+    "revision": "6d654d594d147bf71cb6",
+    "url": "/fashe-shop-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b6b7a915e60d1604a8ae",
+    "url": "/fashe-shop-react/static/css/main.55d7fdcf.chunk.css"
+  },
+  {
+    "revision": "6d654d594d147bf71cb6",
+    "url": "/fashe-shop-react/static/js/2.785e8103.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fashe-shop-react/static/js/2.785e8103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b7a915e60d1604a8ae",
+    "url": "/fashe-shop-react/static/js/main.dbead717.chunk.js"
+  },
+  {
+    "revision": "392ba1a87527b2715c88",
+    "url": "/fashe-shop-react/static/js/runtime-main.6fc2eddf.js"
+  },
+  {
+    "revision": "45e5f62920e4bb3e46b58f9849d15d01",
+    "url": "/fashe-shop-react/static/media/about-lady.45e5f629.jpg"
+  },
+  {
+    "revision": "37abbca113646b8f112af04448b968d9",
+    "url": "/fashe-shop-react/static/media/banner-02.37abbca1.jpg"
+  },
+  {
+    "revision": "d2b4646dae2de015692e8329c6845718",
+    "url": "/fashe-shop-react/static/media/banner-03.d2b4646d.jpg"
+  },
+  {
+    "revision": "2e27560b02242c2f09b3991927118887",
+    "url": "/fashe-shop-react/static/media/banner-04.2e27560b.jpg"
+  },
+  {
+    "revision": "fe9d68d0219a9f5297c9b2f3e39862ef",
+    "url": "/fashe-shop-react/static/media/banner-05.fe9d68d0.jpg"
+  },
+  {
+    "revision": "6e3877bdb1df01f3e46609fe34ddd595",
+    "url": "/fashe-shop-react/static/media/banner-07.6e3877bd.jpg"
+  },
+  {
+    "revision": "b3b570e920d86e99277ec0cca646db57",
+    "url": "/fashe-shop-react/static/media/bg-01.b3b570e9.jpg"
+  },
+  {
+    "revision": "b87e1b6eab8a60335c1ea912558d6ffa",
+    "url": "/fashe-shop-react/static/media/blog-01.b87e1b6e.jpg"
+  },
+  {
+    "revision": "a39ab289dc0b141ae2f5524867455b87",
+    "url": "/fashe-shop-react/static/media/blog-02.a39ab289.jpg"
+  },
+  {
+    "revision": "f01203b9220e72d13915e111012f925b",
+    "url": "/fashe-shop-react/static/media/blog-03.f01203b9.jpg"
+  },
+  {
+    "revision": "50a98e0c664aaebd2607fe619656efa1",
+    "url": "/fashe-shop-react/static/media/glass.50a98e0c.jpg"
+  },
+  {
+    "revision": "72c6d01e91b6bb8f77627622c8906a8d",
+    "url": "/fashe-shop-react/static/media/heading-pages-02.72c6d01e.jpg"
+  },
+  {
+    "revision": "c22b42df2c029c079a59f720eb9653ab",
+    "url": "/fashe-shop-react/static/media/heading-pages-06.c22b42df.jpg"
+  },
+  {
+    "revision": "750e75705032bd0e405936c37b289408",
+    "url": "/fashe-shop-react/static/media/item-02.750e7570.jpg"
+  },
+  {
+    "revision": "3531afeb88ca77a75063a4566cdd5621",
+    "url": "/fashe-shop-react/static/media/item-03.3531afeb.jpg"
+  },
+  {
+    "revision": "96d0c4ef2b8be9b839122185ceb4cd7d",
+    "url": "/fashe-shop-react/static/media/item-05.96d0c4ef.jpg"
+  },
+  {
+    "revision": "bdef4228b9f4560c9154e34afa60a03f",
+    "url": "/fashe-shop-react/static/media/item-06.bdef4228.jpg"
+  },
+  {
+    "revision": "84949f3eadeadc9ca9c6fdeba30bcf68",
+    "url": "/fashe-shop-react/static/media/item-07.84949f3e.jpg"
+  },
+  {
+    "revision": "5494fc9f3197f40e998b113ad0d6e405",
+    "url": "/fashe-shop-react/static/media/item-08.5494fc9f.jpg"
+  },
+  {
+    "revision": "0def7768c1664aee6f24b3b19c9ec230",
+    "url": "/fashe-shop-react/static/media/item-10.0def7768.jpg"
+  },
+  {
+    "revision": "39ec70ef25f686ad8aff51d3a1a6f21d",
+    "url": "/fashe-shop-react/static/media/item-11.39ec70ef.jpg"
+  },
+  {
+    "revision": "88a66c39bec2ddd26b5e55173e7acd3a",
+    "url": "/fashe-shop-react/static/media/item-12.88a66c39.jpg"
+  },
+  {
+    "revision": "fc94b93bd20a9f4d0e40051653e1810e",
+    "url": "/fashe-shop-react/static/media/lady.fc94b93b.jpg"
+  },
+  {
+    "revision": "33f368544e7be8aa2029726c649851fb",
+    "url": "/fashe-shop-react/static/media/master-slide-04.33f36854.jpg"
+  }
+]);
